e4ecb3166546f813629ec661170bffb4eed5a9f8
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-06-25 20:03:51 -0400

    Update quickCalendarInvite.js

--- quickCalendarInvite.js
+++ quickCalendarInvite.js
@@ -1,4 +1,4 @@
 var p = prompt('enter your invitation here: e.g., "Meeting with Andre on Monday at 9am @ Perimeter Point Starbucks"'); 
 if(p != ''){
-window.open('<https://script.google.com/macros/s/<YOURscriptWEBappID>'+'?output='+p);
+window.open('https://script.google.com/macros/s/<YOURscriptWEBappID>'+'?output='+p);
 }
